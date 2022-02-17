03861422940c92576981240b02d49eb00c30f19c
BT_1E10_17 <sample@bths.edu>
2022-02-17 13:56:15 -0500

    Added solutions to the various methods, added masterfile word txt

--- src/Sort.java
+++ src/Sort.java
@@ -3,49 +3,97 @@ import java.util.ArrayList;
 public class Sort
 {
   // Part A.  Which Sort is Which?
-  public static void firstSortingAlgorithm(int[] elements)
+  public static int selectionSort(int[] elements)
   {
+    int counter = 0;
     for (int j = 0; j < elements.length - 1; j++)
     {
       int minIndex = j;
       for (int k = j + 1; k < elements.length; k++)
-      {      
+      {
+        counter++;
         if (elements[k] < elements[minIndex])
         {
           minIndex = k;
         }
       }
+
+      //replacement/swapping
       int temp = elements[j];
       elements[j] = elements[minIndex];
       elements[minIndex] = temp;
+
     }
+
+    return counter;
   }
 
   // Part A.  Which Sort is Which?
-  public static void secondSortingAlgorithm(int[] elements)
-  { 
+  public static int insertionSort(int[] elements)
+  {
+    int counter = 0;
     for (int j = 1; j < elements.length; j++)
     {
       int temp = elements[j];
       int possibleIndex = j;
       while (possibleIndex > 0 && temp < elements[possibleIndex - 1])
-      {     
+      {
+        counter++;
         elements[possibleIndex] = elements[possibleIndex - 1];
         possibleIndex--;
+
       }
       elements[possibleIndex] = temp;
+
     }
+    return counter;
   }
   
   // Part C.  Sorting a 1000-word list!
   public static void selectionSortWordList(ArrayList<String> words)
   {
-    /* TO BE IMPLEMENTED IN PART C */
+    int counter = 0;
+    for (int j = 0; j < words.size()- 1; j++)
+    {
+      int minIndex = j; // minimum idx that will updated as a new minimum is found
+      for (int k = j + 1; k < words.size(); k++)
+      {
+        counter++;
+        if (words.get(k).compareTo(words.get(minIndex)) < 0) //comparing current value to current minimum
+        {
+          minIndex = k;
+        }
+      }
+      //replacement/swapping
+      String temp = words.get(j);
+      words.set(j, words.get(minIndex));
+      words.set(minIndex, temp);
+    }
+    System.out.println("--------------------------------------");
+    System.out.println("Selection sort iteration loop counter: " + counter);
+    System.out.println("--------------------------------------");
+
+
   }
 
   // Part C.  Sorting a 1000-word list!
   public static void insertionSortWordList(ArrayList<String> words)
-  {    
-    /* TO BE IMPLEMENTED IN PART C */
+  {
+    int counter = 0;
+    for (int j = 1; j < words.size(); j++)
+    {
+      String temp = words.get(j);
+      int idx = j;
+      while (idx > 0 && temp.compareTo(words.get(idx -1)) < 0)
+      {
+        counter++;
+        words.set(idx, words.get(idx-1));
+        idx--;
+      }
+      words.set(idx, temp);
+    }
+    System.out.println("--------------------------------------");
+    System.out.println("Insertion sort iteration loop counter: " + counter);
+    System.out.println("--------------------------------------");
   }
 }
--- src/SortTester.java
+++ src/SortTester.java
@@ -5,13 +5,17 @@ public class SortTester
   public static void main(String[] args)
   {
     /* --- SELECTION SORT --- */
-    int[] arr1 = {3, 86, -20, 14, 40, 19, -4, 100, 89, 13, 54, 12, 77, -5, 0, 6}; 
-    Sort.selectionSort(arr1);
+    int[] arr1 = {72, 72, 72, 72, 72, 72, 72, 72, 72, 72, 72, 72, 72, 72, 72, 72, 72, 72, 72, 72, 72, 72, 72, 72};
+      System.out.println(arr1.length);
+    int counter1 = Sort.selectionSort(arr1);
     System.out.println(Arrays.toString(arr1));
+    System.out.println("Selection sort iteration counter: " + counter1);
  
     /* --- INSERTION SORT --- */ 
-    int[] arr2 = {3, 86, -20, 14, 40, 19, -4, 100, 89, 13, 54, 12, 77, -5, 0, 6};    
-    Sort.insertionSort(arr2);
+    int[] arr2 = {72, 72, 72, 72, 72, 72, 72, 72, 72, 72, 72, 72, 72, 72, 72, 72, 72, 72, 72, 72, 72, 72, 72, 72};
+    System.out.println(arr2.length);
+    int counter2 = Sort.insertionSort(arr2);
     System.out.println(Arrays.toString(arr2));
+    System.out.println("Selection sort iteration counter: " + counter2);
   }
 }
